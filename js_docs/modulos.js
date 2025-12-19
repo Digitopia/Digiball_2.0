caa1630a3c2dc5e20c7793bc01289af76006a3d4
EmaFerreira <82063617+EmaFerreira@users.noreply.github.com>
2025-12-19 10:26:58 +0000

    Update modulos.js

--- js_docs/modulos.js
+++ js_docs/modulos.js
@@ -120,7 +120,7 @@ function drone() {
 
 let estado_drone = false;
 let d;
-let full_sim = true; //alterado temporáriamente para testar android. Voltar a colocar cm false
+let full_sim = false; //alterar temporáriamente para true p testar android. Voltar a colocar cm false no fim 
 
 /* PARA ATIVAR E DESATIVAR SIMULAÇÕES: CARREGAR TECLAS  */
 function keyPressed() {
